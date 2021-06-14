fda83749e98e37872095d82adefa7f4e3ad7b922
Dima <rootcfg@mail.ru>
2021-06-14 06:46:52 +0000

    favicon added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("semantic-ui-sass")
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
